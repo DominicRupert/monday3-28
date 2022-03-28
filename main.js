f5a5c6ca6d7f1f93b12778771bc6df948633088f
dominic rupert <dominicrupert@gmail.com>
2022-03-28 15:48:05 -0600

    new

--- main.js
+++ main.js
@@ -1,10 +1,52 @@
-let score=0
-function scoreUp() {
+let score = 0
+
+
+
     
-    score++
-    console.log(score);
-}
-function scoreDown(){
-    score --
+    function scoreUp() {
+        
+        score++
+        
+        if (score==5) { 
+            document.getElementById('score-goal').innerHTML='<img src="..//images/classic_kirby_3d_model_by_milesgamesda_deud7pd-fullview.png" alt=""></img>'
+            document.getElementById('score-board').innerText="you scored 5"
+        }
+       
+        if (score==10) {
+            document.getElementById('score-goal').innerHTML='<img src="..//images/kirby.png" alt="">'
+            document.getElementById('score-board').innerText="you scored 10"
+           
+        }
+     
+    }
+    function scoreDown(){
+        score--
+       
+        if (score == -5){
+            
+            document.getElementById('score-goal').innerText=':('
+            document.getElementById('score-board').innerText=':('
+        }
+        if (score == -10){
+            
+            document.getElementById('score-goal').innerText=':(('
+            document.getElementById('score-board').innerText=':(('
+        }
+       
+        
+    }
+  
+    
+function drawscore(){
+
+    
+    if (score <= 0){
+        document.getElementById('score-display').innerText=score
+        
+    }
+    if (score >= 0){
+        document.getElementById('score-display').innerText=score
+        
+    }
     console.log(score);
 }
